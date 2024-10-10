1f4a1dee6d9b0932d25fa84c5ed8ae20bc4a3524
Farhan-rashid10 <farkaf07@gmail.com>
2024-10-10 18:52:26 +0300

    still

--- project/src/Home.js
+++ project/src/Home.js
@@ -4,14 +4,17 @@ import AboutSection from './AboutSection';
 import Forms from './Forms';
 import StatsSection from './Section';
 import ImageLayout from './Carousel';
+import MainServices from './MainServices';
 const HomePage = () => {
   return (
     <div className="bg-gray-50">
       <Forms />
+      <MainServices />
       <FeaturesSection />
       <StatsSection />
       <AboutSection />
       <ImageLayout />
+
     </div>
   );
 };
